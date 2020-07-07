176128a8a0bfb57661abc7105b0851d20750408a
Junha Lee <junhakiwi@postech.ac.kr>
2020-07-07 15:24:29 +0900

    install zsh script

---
+++ install-zsh.sh
@@ -0,0 +1,10 @@
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Symlink zshrc
+mv ~/.zshrc ~/.zshrc.old
+ln -s ~/dotfiles/zshrc ~/.zshrc
