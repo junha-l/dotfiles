742f526c0c332850c6aea4c06276507537fbd4d4
Junha Lee <junhakiwi@postech.ac.kr>
2020-07-08 13:03:53 +0900

    minor fix

--- install-tmux.sh
+++ install-tmux.sh
@@ -2,4 +2,5 @@
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
+echo "set-option -g default-shell /usr/bin/zsh" >> .tmux/.tmux.conf.local
 cp .tmux/.tmux.conf.local .
--- install-zsh.sh
+++ install-zsh.sh
@@ -5,3 +5,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Setup
+sed -i "s|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting)|g" ~/.zshrc
+echo "alias vim=\"nvim\"">> ~/.zshrc
+echo "alias vi=\"nvim\"">> ~/.zshrc
