e83ecb60284e301e3712e62799e310f232548b08
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-30 14:12:37 +0800

    Delete install-tmux.sh

--- install-tmux.sh
+++
@@ -1,6 +0,0 @@
-# Install tmux
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-echo "set-option -g default-shell /usr/bin/zsh" >> .tmux/.tmux.conf.local
-cp .tmux/.tmux.conf.local .
